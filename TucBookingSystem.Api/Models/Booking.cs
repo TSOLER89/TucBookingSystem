da4247d8e039028d113ecb403b52733b190c809d
Marachinos <stallnysater@gmail.com>
2026-03-11 12:34:46 +0100

    Models med Booking.cs, User.cs och Room.cs. Ändrat namn på BookingUSer till User och Rum till Room.

--- TucBookingSystem.Api/Models/Booking.cs
+++ TucBookingSystem.Api/Models/Booking.cs
@@ -1,6 +1,24 @@
-﻿namespace TucBookingSystem.Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TucBookingSystem.Api.Models
 {
     public class Booking
     {
+        public int Id { get; set; }
+
+        [Required]
+        public int RoomId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public DateTime StartTime { get; set; }
+
+        [Required]
+        public DateTime EndTime { get; set; }
+
+        public Room? Room { get; set; }
+        public User? User { get; set; }
     }
 }
---
+++ TucBookingSystem.Api/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TucBookingSystem.Api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FullName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        [Required]
+        public string Role { get; set; } = "User";
+    }
+}
