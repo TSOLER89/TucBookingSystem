44e796eab00d5afddb78db1910ccb73700a3e676
Aigen80 <aomurm@gmail.com>
2026-03-11 13:23:29 +0100

    Lade till repository- och service-registreringar i Program.cs

--- TucBookingSystem.Api/Repositories/BookingRepository.cs
+++ TucBookingSystem.Api/Repositories/BookingRepository.cs
@@ -1,6 +1,48 @@
-﻿namespace TucBookingSystem.Api.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TucBookingSystem.Api.Data;
+using TucBookingSystem.Api.Models;
+
+namespace TucBookingSystem.Api.Repositories;
+
+public class BookingRepository : IBookingRepository
 {
-    public class BookingRepository
+    private readonly AppDbContext _context;
+
+    public BookingRepository(AppDbContext context)
     {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Booking>> GetAllAsync() =>
+        await _context.Bookings.Include(b => b.User).Include(b => b.Room).ToListAsync();
+
+    public async Task<Booking?> GetByIdAsync(int id) =>
+        await _context.Bookings.Include(b => b.User).Include(b => b.Room).FirstOrDefaultAsync(b => b.Id == id);
+
+    public async Task<IEnumerable<Booking>> GetByUserIdAsync(string userId) =>
+        await _context.Bookings.Where(b => b.UserId == userId).Include(b => b.Room).ToListAsync();
+
+    public async Task AddAsync(Booking booking)
+    {
+        await _context.Bookings.AddAsync(booking);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Booking booking)
+    {
+        _context.Bookings.Update(booking);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var booking = await _context.Bookings.FindAsync(id);
+        if (booking != null)
+        {
+            _context.Bookings.Remove(booking);
+            await _context.SaveChangesAsync();
+        }
     }
 }
--- TucBookingSystem.Api/Repositories/IBookingRepository.cs
+++ TucBookingSystem.Api/Repositories/IBookingRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace TucBookingSystem.Api.Repositories
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TucBookingSystem.Api.Repositories;
+
+public interface IBookingRepository
 {
-    public class IBookingRepository
-    {
-    }
+    Task<IEnumerable<Booking>> GetAllAsync();
+    Task<Booking?> GetByIdAsync(int id);
+    Task<IEnumerable<Booking>> GetByUserIdAsync(string userId);
+    Task AddAsync(Booking booking);
+    Task UpdateAsync(Booking booking);
+    Task DeleteAsync(int id);
 }
