7bc996e1d7c97b8e615395dfaf5d2d5993fc18d1
MarikaRomeo <marika.romeo.syne25lin@edu.tucsweden.se>
2026-03-11 12:26:19 +0100

    Update ApplicationDbContext with entities and relationships
    Refactored ApplicationDbContext to inherit from DbContext, added DbSet properties for ApplicationUser, Room, and Booking. Implemented constructor and OnModelCreating to configure unique index on user email and set up foreign key relationships. Added necessary using statements.

--- TucBookingSystem.Api/Data/ApplicationDbContext.cs
+++ TucBookingSystem.Api/Data/ApplicationDbContext.cs
@@ -1,6 +1,61 @@
 ﻿namespace TucBookingSystem.Api.Data
 {
-    public class ApplicationDbContext
+    using global::TucBookingSystem.Api.Models;
+    using Microsoft.EntityFrameworkCore;
+    using System.Reflection.Emit;
+    using TucBookingSystem.Api.Models;
+
+    namespace TucBookingSystem.Api.Data
     {
+
+        public class ApplicationDbContext : DbContext
+
+        {
+
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+
+                : base(options)
+
+            {
+
+            }
+
+            public DbSet<ApplicationUser> Users => Set<ApplicationUser>();
+
+            public DbSet<Room> Rooms => Set<Room>();
+
+            public DbSet<Booking> Bookings => Set<Booking>();
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+
+            {
+
+                base.OnModelCreating(modelBuilder);
+
+                modelBuilder.Entity<ApplicationUser>()
+
+                    .HasIndex(u => u.Email)
+
+                    .IsUnique();
+
+                modelBuilder.Entity<Booking>()
+
+                    .HasOne(b => b.Room)
+
+                    .WithMany(r => r.Bookings)
+
+                    .HasForeignKey(b => b.RoomId);
+
+                modelBuilder.Entity<Booking>()
+
+                    .HasOne(b => b.User)
+
+                    .WithMany(u => u.Bookings)
+
+                    .HasForeignKey(b => b.UserId);
+
+            }
+        }
     }
+
 }
