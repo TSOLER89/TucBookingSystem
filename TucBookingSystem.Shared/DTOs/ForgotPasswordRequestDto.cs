e7737a5878cd486d57e71693f61354bcb692165a
jennykhranovska <jenny.khranovska@gmail.com>
2026-03-16 16:04:05 +0100

    Add ForgotPasswordRequestDto class for password reset
    Introduced ForgotPasswordRequestDto as an internal class in the Shared.DTOs namespace. This DTO includes an Email property to support password reset functionality. Added necessary using directives.

---
+++ TucBookingSystem.Shared/DTOs/ForgotPasswordRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TucBookingSystem.Shared.DTOs
+{
+    internal class ForgotPasswordRequestDto
+    {
+        public string Email { get; set; } = "";
+    }
+}
