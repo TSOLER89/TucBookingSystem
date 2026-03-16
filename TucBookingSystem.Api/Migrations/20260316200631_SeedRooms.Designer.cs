df9f3fd80888e39722ae6d37755020a1aa9ac588
Marachinos <stallnysater@gmail.com>
2026-03-16 21:13:04 +0100

    Har lagt till Alla Rum i databasen. Att rummen finns i Linköping och hur många som får plats i de olika rummen.

--- TucBookingSystem.Api/Migrations/20260316200631_SeedRooms.Designer.cs
+++ TucBookingSystem.Api/Migrations/20260316200631_SeedRooms.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TucBookingSystem.Api.Data;
 
@@ -10,9 +11,11 @@ using TucBookingSystem.Api.Data;
 namespace TucBookingSystem.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260316200631_SeedRooms")]
+    partial class SeedRooms
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.5");
