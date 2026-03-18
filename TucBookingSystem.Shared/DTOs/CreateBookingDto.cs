12fa6dbc3e20d1f002d00755dfda6e66e392ae1b
AigennA <aomurm@gmail.com>
2026-03-18 11:22:05 +0100

    Added Data Annotations to DTOs

--- TucBookingSystem.Shared/DTOs/CreateBookingDto.cs
+++ TucBookingSystem.Shared/DTOs/CreateBookingDto.cs
@@ -1,10 +1,22 @@
-﻿namespace TucBookingSystem.Shared.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TucBookingSystem.Shared.DTOs;
 
 public class CreateBookingDto
 {
+    [Required]
+    [Range(1, int.MaxValue, ErrorMessage = "Du måste välja ett rum.")]
     public int RoomId { get; set; }
+
+    [Required]
     public DateOnly Date { get; set; }
+
+    [Required]
     public TimeOnly StartTime { get; set; }
+
+    [Required]
     public TimeOnly EndTime { get; set; }
+
+    [MaxLength(200)]
     public string Purpose { get; set; } = string.Empty;
 }
--- TucBookingSystem.Shared/DTOs/ResetPasswordRequestDto.cs
+++ TucBookingSystem.Shared/DTOs/ResetPasswordRequestDto.cs
@@ -1,13 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace TucBookingSystem.Shared.DTOs
 {
     public class ResetPasswordRequestDto
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; } = "";
+
+        [Required]
         public string Token { get; set; } = "";
+
+        [Required]
+        [MinLength(6, ErrorMessage = "Lösenordet måste vara minst 6 tecken.")]
         public string NewPassword { get; set; } = "";
     }
 }
